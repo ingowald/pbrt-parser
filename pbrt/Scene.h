2491f2782ac4455650f7efbf79d823939c605729
Ingo Wald <ingowald@gmail.com>
2017-01-31 12:39:26 -0700

    more link error fixes for windows

--- pbrt/Scene.h
+++ pbrt/Scene.h
@@ -183,15 +183,15 @@ namespace pbrt_parser {
 
   //! what's in a objectbegin/objectned, as well as the root object
   struct PBRT_PARSER_INTERFACE Object {
-    PBRT_PARSER_INTERFACE Object(const std::string &name) : name(name) {}
-
+    Object(const std::string &name) : name(name) {}
+    
     struct PBRT_PARSER_INTERFACE Instance {
-      PBRT_PARSER_INTERFACE Instance(const std::shared_ptr<Object> &object,
+      Instance(const std::shared_ptr<Object> &object,
                const affine3f    &xfm)
         : object(object), xfm(xfm)
       {}
-
-      PBRT_PARSER_INTERFACE std::string toString() const;
+      
+      std::string toString() const;
 
       std::shared_ptr<Object> object;
       affine3f    xfm;
@@ -214,10 +214,9 @@ namespace pbrt_parser {
 
   struct PBRT_PARSER_INTERFACE Scene {
 
-    PBRT_PARSER_INTERFACE Scene()
-      {
-        world = std::make_shared<Object>("<root>");
-      };
+    Scene() {
+      world = std::make_shared<Object>("<root>");
+    };
 
     //! list of cameras defined in this object
     std::vector<std::shared_ptr<Camera> > cameras;
